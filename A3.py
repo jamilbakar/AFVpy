aac6bd6d031643511247993c935ce8e2de53d982
jamilbakar <jabakar@chapman.edu>
2026-06-19 20:25:21 +0000

    Jun 19, 2026, 1:25 PM

--- A3.py
+++ A3.py
@@ -1,12 +1,5 @@
-try:
-    import numpy as np
-except ImportError as e:
-    raise ImportError("numpy is required to run A3.py. Install it with: pip install numpy") from e
-
-try:
-    from scipy.stats import multivariate_normal
-except ImportError as e:
-    raise ImportError("scipy is required to run A3.py. Install it with: pip install scipy") from e
+import numpy as np
+from scipy.stats import multivariate_normal
 class MvGaussian:
     # (env::MvGaussian)(s, a) = s
     def step(self, s, a):
