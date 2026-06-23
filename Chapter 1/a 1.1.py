e39e93c47ee5d67ad1374dd0afb98365a2dcce62
jamilbakar <jabakar@chapman.edu>
2026-06-23 16:57:05 +0000

    Jun 23, 2026, 9:57 AM

---
+++ Chapter 1/a 1.1.py
@@ -0,0 +1,19 @@
+# Algorithm 1.1: a system = an agent, its environment, and its sensor(s).
+
+class Agent:
+    pass
+
+
+class Environment:
+    pass
+
+
+class Sensor:
+    pass
+
+
+class System:
+    def __init__(self, agent, env, sensor):
+        self.agent = agent
+        self.env = env
+        self.sensor = sensor
